9a239cfbddfcf8df15a57fa0d0e716ec2111d13d
Márcio Daniel <marcio.daniel@msn.com>
2023-01-30 22:42:19 -0300

    feat: resolve exercise 4

---
+++ B.E. 4/4.1/exercicio4.js
@@ -0,0 +1,21 @@
+const readlineSync = require('readline-sync');
+
+const getDataInput = () => {
+  const weight = readlineSync.questionFloat('What’s your weight?\n');
+  const height = readlineSync.questionFloat('What’s your height??\n');
+  return { weight, height };
+};
+
+const calculateBMI = (weight, height) => {
+  const BMI = weight / (height * height);
+  return BMI;
+};
+
+const worker = () => {
+  const { weight, height } = getDataInput();
+  console.log(typeof weight, typeof height);
+  const BMI = calculateBMI(weight, height);
+  console.log(BMI);
+};
+
+worker();
